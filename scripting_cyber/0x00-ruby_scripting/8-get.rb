46391ac0d783857b91ebae959a7e01e3c2fe1003
Theemiss <midinfotn401@gmail.com>
2024-07-05 16:41:01 +0100

    task 8

--- scripting_cyber/0x00-ruby_scripting/8-get.rb
+++ scripting_cyber/0x00-ruby_scripting/8-get.rb
@@ -1,11 +1,17 @@
 require 'net/http'
 require 'uri'
+require 'json'
 
 def get_request(url)
   uri = URI.parse(url)
   
   response = Net::HTTP.get_response(uri)
-  puts "Response Status Code: #{response.code}"
-  puts "Response Body:"
-  puts response.body
+  status_code = response.code
+  status_message = response.message
+  body = response.body
+  
+  puts "Response status: #{status_code} #{status_message}"
+  puts "Response body:"
+  puts JSON.pretty_generate(JSON.parse(body))
 end
+
